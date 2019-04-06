81599e70314a137e98fa86730cb862ac3ce9cf2d
mreyes9406 <mreyes9406@gmail.com>
2019-04-06 12:00:57 -0600

    Finished backend

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def selected_period_info(selected_date_1, selected_date_2):
                                    AVG(CAST(precio_perdida_mtr AS DECIMAL)) AS precio_perdida_mtr_prom,\
                                    AVG(CAST(precio_congestion_mtr AS DECIMAL)) AS precio_congestion_mtr_prom\
                                  FROM prices_table\
-                                 WHERE fecha BETWEEN DATE('" + selected_date_1 + "') AND DATE('" + selected_date_2 + "')\
+                                 WHERE fecha BETWEEN DATE('" + selected_date_1 + "') AND DATE('" + selected_date_2 + "') AND sistema = 'BCA'\
                                  GROUP BY fecha, zona_carga, sistema\
                                  ORDER BY sistema, fecha"
                                 )
@@ -62,6 +62,7 @@ def selected_period_info(selected_date_1, selected_date_2):
 
     period_data = {
         'period_days':period_days,
+        'zones in system':zonas,
         'dates':{
             day:{
                 zone:{
@@ -85,7 +86,7 @@ def selected_period_info(selected_date_1, selected_date_2):
                                    AVG(CAST(precio_perdida_mtr AS DECIMAL)) AS precio_perdida_mtr_prom,\
                                    AVG(CAST(precio_congestion_mtr AS DECIMAL)) AS precio_congestion_mtr_prom\
                                  FROM prices_table\
-                                 WHERE fecha BETWEEN DATE('" + selected_date_1 + "') AND DATE('" + selected_date_2 + "')\
+                                 WHERE fecha BETWEEN DATE('" + selected_date_1 + "') AND DATE('" + selected_date_2 + "') AND sistema = 'BCA'\
                                  GROUP BY fecha, zona_carga, sistema\
                                  ORDER BY sistema, fecha"
                                 )
@@ -101,8 +102,8 @@ def selected_period_info(selected_date_1, selected_date_2):
         
     return jsonify(period_data)
 
-@app.route("/date/<date>")
-def selected_day_info(date):
+@app.route("/period/<selected_date_1>/<selected_date_2>/day/<date>")
+def selected_day_info(selected_date_1, selected_date_2, date):
 
     precios = ['precio_mda_promedio', 'precio_energia_mda_promedio', 'precio_perdida_mda_promedio', 'precio_congestion_mda_promedio',
                'precio_mtr_promedio', 'precio_energia_mtr_promedio', 'precio_perdida_mtr_promedio', 'precio_congestion_mtr_promedio']
@@ -124,7 +125,7 @@ def selected_day_info(date):
                                    AVG(CAST(precio_perdida_mtr AS DECIMAL)) AS precio_perdida_mtr_prom,\
                                    AVG(CAST(precio_congestion_mtr AS DECIMAL)) AS precio_congestion_mtr_prom\
                                  FROM prices_table\
-                                 WHERE fecha = DATE('" + date + "')"
+                                 WHERE fecha = DATE('" + date + "')  AND sistema = 'BCA'"
     )
 
     for row in table_2:
@@ -139,33 +140,50 @@ def selected_day_info(date):
 
     return jsonify(day_data)
 
-@app.route("/zone/<zone>")
-def selected_period_days(zone):
+@app.route("/period/<selected_date_1>/<selected_date_2>/day/<date>/zone/<zone>")
+def selected_period_days(selected_date_1, selected_date_2, date, zone):
 
-    precios = ['precio_mda_promedio', 'precio_mtr_promedio']
+    precios = ['precio_mda_promedio', 'precio_mtr_promedio','precio_mda_max', 'precio_mtr_max']
+
+    week_table = engine.execute(
+                                "SELECT DISTINCT\
+                                   fecha\
+                                FROM prices_table\
+                                WHERE DATE(fecha) BETWEEN DATE ('" + date + "', '-6 days') AND DATE('" + date + "')"
+    )
+
+    week_days = []
+    for day in week_table:
+        week_days.append(day[0])
 
     zone_data = {
-                'zone':zone,
-                 date: {
-                     precio:[] for precio in precios
-                 }
+        day:{
+            precio:[] for precio in precios 
+        } for day in week_days
              }
+
+    zone_data['zona'] = zone
+
     table_3 = engine.execute(
                                 "SELECT\
                                    zona_carga,\
                                    fecha,\
                                    AVG(CAST(precio_mda AS DECIMAL)) AS precio_mda_prom,\
-                                   AVG(CAST(precio_mtr AS DECIMAL)) AS precio_mtr_prom\
+                                   AVG(CAST(precio_mtr AS DECIMAL)) AS precio_mtr_prom,\
+                                   MAX(CAST(precio_mda AS DECIMAL)) AS precio_mda_max,\
+                                   MAX(CAST(precio_mtr AS DECIMAL)) AS precio_mtr_max\
                                 FROM prices_table\
-                                WHERE DATE(fecha) BETWEEN DATE ('" + date + "', '-6 days') AND DATE('" + date + "') AND zona_carga = " + zone + "\
+                                WHERE DATE(fecha) BETWEEN DATE ('" + date + "', '-6 days') AND DATE('" + date + "') AND zona_carga = '" + zone + "'\
                                 GROUP BY zona_carga, fecha"
     )
 
     for row in table_3:
-        zone_data[date]['precio_mda_promedio'].append(row[2])
-        zone_data[date]['precio_mtr_promedio'].append(row[3])
+        zone_data[row[1]]['precio_mda_promedio'].append(row[2])
+        zone_data[row[1]]['precio_mtr_promedio'].append(row[3])
+        zone_data[row[1]]['precio_mda_max'].append(row[4])
+        zone_data[row[1]]['precio_mtr_max'].append(row[5])
 
-    return jsionfy(zone_data)
+    return jsonify(zone_data)
 
 if __name__ == "__main__":
     app.run()
